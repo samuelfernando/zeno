acae6671aeb45fa4ae7d074fcf37798bd48caab4
zeno <zeno@192.168.0.55>
2014-02-16 12:11:30 +0000

    updated

--- src/main/java/uk/ac/shef/basic/MySensorTest.java
+++ src/main/java/uk/ac/shef/basic/MySensorTest.java
@@ -1,5 +1,10 @@
 package uk.ac.shef.basic;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.robokind.client.basic.*;
 import org.jflux.api.core.Listener;
 import org.robokind.api.sensor.*;
@@ -13,89 +18,97 @@ public class MySensorTest {
     
     
      public static void main(String[] args) {
-         String ip = "192.168.0.54";
-        UserSettings.setSensorAddress(ip);
-        UserSettings.setAccelerometerAddress(ip);
-        UserSettings.setGyroscopeAddress(ip);
-        UserSettings.setCompassAddress(ip);
-        UserSettings.setRobotId("myRobot");
-        
-        RemoteGpioServiceClient sensors = Robokind.connectSensors();
-        DeviceReadPeriodEvent<HeaderRecord> readPeriod =
-                new DeviceReadPeriodRecord();
-        HeaderRecord header = new HeaderRecord();
-        
-        System.out.println("Adding pin direction.");
-        
-        sensors.setPinDirection(2, true);
-        sensors.setPinDirection(4, true);
-        
-        System.out.println("Adding read period.");
-        
-        header.setFrameId(0);
-        header.setSequenceId(0);
-//        header.setTimestamp(TimeUtils.now());
-        header.setTimestamp(0L);
-        readPeriod.setHeader(header);
-        readPeriod.setPeriod(100.0);
-        
-        sensors.setReadPeriod(readPeriod);
-        
-        System.out.println("Adding listener.");
-        
-        sensors.addListener(new TestGpioListener());
-        
-        System.out.println("Adding IMU.");
-        
-        RemoteAccelerometerServiceClient accel =
-                Robokind.connectAccelerometer();
-        RemoteGyroscopeServiceClient gyro = Robokind.connectGyroscope();
-        RemoteCompassServiceClient compass = Robokind.connectCompass();
-        
-        readPeriod.setPeriod(1000.0);
-        
-        accel.setReadPeriod(readPeriod);
-        gyro.setReadPeriod(readPeriod);
-        compass.setReadPeriod(readPeriod);
-        
-        AccelerometerConfigRecord accelConfig = new AccelerometerConfigRecord();
-        GyroConfigRecord gyroConfig = new GyroConfigRecord();
-        CompassConfigRecord compassConfig = new CompassConfigRecord();
-        
-        accelConfig.setHeader(header);
-        gyroConfig.setHeader(header);
-        compassConfig.setHeader(header);
-        
-        accelConfig.setRegisterAddress(45);
-        accelConfig.setRegisterValue(8);
-        
-        gyroConfig.setCtl1(15);
-        gyroConfig.setCtl2(-1);
-        gyroConfig.setCtl3(-1);
-        gyroConfig.setCtl4(-1);
-        gyroConfig.setCtl5(-1);
-        
-        compassConfig.setAverage(3);
-        compassConfig.setBias(0);
-        compassConfig.setGain(7);
-        compassConfig.setRate(2);
-        
-        accel.sendConfig(accelConfig);
-        gyro.sendConfig(gyroConfig);
-        compass.sendConfig(compassConfig);
-        
-        accel.addListener(new TestAccelListener());
-        gyro.addListener(new TestGyroListener());
-        compass.addListener(new TestCompassListener());
-        int count = 0;
-        while (true) {
-            try {
-                Thread.sleep(1000);
-            } catch(InterruptedException ex) {
-                Thread.currentThread().interrupt();
+         try {
+             BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\zeno\\Documents\\NetBeansProjects\\zeno-ip.txt"));
+                String ip = br.readLine();
+                System.out.println("ip = "+ip);
+             
+             //String ip = "192.168.0.54";
+            UserSettings.setSensorAddress(ip);
+            UserSettings.setAccelerometerAddress(ip);
+            UserSettings.setGyroscopeAddress(ip);
+            UserSettings.setCompassAddress(ip);
+            UserSettings.setRobotId("myRobot");
+            
+            RemoteGpioServiceClient sensors = Robokind.connectSensors();
+            DeviceReadPeriodEvent<HeaderRecord> readPeriod =
+                    new DeviceReadPeriodRecord();
+            HeaderRecord header = new HeaderRecord();
+            
+            System.out.println("Adding pin direction.");
+            
+            sensors.setPinDirection(2, true);
+            sensors.setPinDirection(4, true);
+            
+            System.out.println("Adding read period.");
+            
+            header.setFrameId(0);
+            header.setSequenceId(0);
+    //        header.setTimestamp(TimeUtils.now());
+            header.setTimestamp(0L);
+            readPeriod.setHeader(header);
+            readPeriod.setPeriod(100.0);
+            
+            sensors.setReadPeriod(readPeriod);
+            
+            System.out.println("Adding listener.");
+            
+            sensors.addListener(new TestGpioListener());
+            
+            System.out.println("Adding IMU.");
+            
+            RemoteAccelerometerServiceClient accel =
+                    Robokind.connectAccelerometer();
+            RemoteGyroscopeServiceClient gyro = Robokind.connectGyroscope();
+            RemoteCompassServiceClient compass = Robokind.connectCompass();
+            
+            readPeriod.setPeriod(1000.0);
+            
+            accel.setReadPeriod(readPeriod);
+            gyro.setReadPeriod(readPeriod);
+            compass.setReadPeriod(readPeriod);
+            
+            AccelerometerConfigRecord accelConfig = new AccelerometerConfigRecord();
+            GyroConfigRecord gyroConfig = new GyroConfigRecord();
+            CompassConfigRecord compassConfig = new CompassConfigRecord();
+            
+            accelConfig.setHeader(header);
+            gyroConfig.setHeader(header);
+            compassConfig.setHeader(header);
+            
+            accelConfig.setRegisterAddress(45);
+            accelConfig.setRegisterValue(8);
+            
+            gyroConfig.setCtl1(15);
+            gyroConfig.setCtl2(-1);
+            gyroConfig.setCtl3(-1);
+            gyroConfig.setCtl4(-1);
+            gyroConfig.setCtl5(-1);
+            
+            compassConfig.setAverage(3);
+            compassConfig.setBias(0);
+            compassConfig.setGain(7);
+            compassConfig.setRate(2);
+            
+            accel.sendConfig(accelConfig);
+            gyro.sendConfig(gyroConfig);
+            compass.sendConfig(compassConfig);
+            
+            accel.addListener(new TestAccelListener());
+            gyro.addListener(new TestGyroListener());
+            compass.addListener(new TestCompassListener());
+            int count = 0;
+            while (true) {
+                try {
+                    Thread.sleep(1000);
+                } catch(InterruptedException ex) {
+                    Thread.currentThread().interrupt();
+                }
+                System.out.println("active "+ (count++));
             }
-            System.out.println("active "+ (count++));
-        }
+         } catch (IOException ex) {
+             Logger.getLogger(MySensorTest.class.getName()).log(Level.SEVERE, null, ex);
+         }
     }
     
     private static class TestGpioListener implements Listener<DeviceBoolEvent> {
--- src/main/java/uk/ac/shef/basic/PDTest.java
+++ src/main/java/uk/ac/shef/basic/PDTest.java
@@ -3,6 +3,16 @@ package uk.ac.shef.basic;
 
 import com.cycling74.max.*;
 import static com.cycling74.max.MaxObject.post;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.robokind.api.common.position.NormalizedDouble;
 import org.robokind.api.motion.Joint;
 import org.robokind.api.motion.Robot;
@@ -16,31 +26,44 @@ public class PDTest extends MaxObject implements Executable {
     private static RemoteRobot myRobot;
     private static Robot.RobotPositionMap myGoalPositions;
     private static RemoteSpeechServiceClient mySpeaker;
+    private static PrintWriter pw;
     public PDTest() {
-        String robotID = "myRobot";
-        String robotIP = "192.168.0.54";
-        
-        //////////////////////////////////////////////////////
-        // SETTINGS - this is handled in SetSettings.java ////
-        //////////////////////////////////////////////////////
-        
-        ///////////// use line 45 ////////////////////////////
-        SetSettings settings = new SetSettings(robotID, robotIP);
-        //////////// instead of 47-55 ////////////////////////
-        UserSettings.setRobotId(robotID);
-        UserSettings.setRobotAddress(robotIP);
-        UserSettings.setAnimationAddress(robotIP);
-        UserSettings.setSpeechAddress(robotIP);
-        UserSettings.setSensorAddress(robotIP);
-        UserSettings.setAccelerometerAddress(robotIP);
-        UserSettings.setGyroscopeAddress(robotIP);
-        UserSettings.setCompassAddress(robotIP);
-        UserSettings.setCameraAddress(robotIP);
-        //////////// End settings //////////////////////////////
-        myRobot = Robokind.connectRobot();
-       
-        myGoalPositions = new Robot.RobotPositionHashMap();
-        mySpeaker = Robokind.connectSpeechService();
+        BufferedReader br = null;
+        try {
+            String robotID = "myRobot";
+            br = new BufferedReader(new FileReader("C:\\Users\\zeno\\Documents\\NetBeansProjects\\zeno-ip.txt"));
+            String robotIP = br.readLine();
+            System.out.println("ip = "+robotIP);
+            //String robotIP = "192.168.0.54";
+            //////////////////////////////////////////////////////
+            // SETTINGS - this is handled in SetSettings.java ////
+            //////////////////////////////////////////////////////
+            ///////////// use line 45 ////////////////////////////
+            SetSettings settings = new SetSettings(robotID, robotIP);
+            //////////// instead of 47-55 ////////////////////////
+            UserSettings.setRobotId(robotID);
+            UserSettings.setRobotAddress(robotIP);
+            UserSettings.setAnimationAddress(robotIP);
+            UserSettings.setSpeechAddress(robotIP);
+            UserSettings.setSensorAddress(robotIP);
+            UserSettings.setAccelerometerAddress(robotIP);
+            UserSettings.setGyroscopeAddress(robotIP);
+            UserSettings.setCompassAddress(robotIP);
+            UserSettings.setCameraAddress(robotIP);
+            //////////// End settings //////////////////////////////
+            myRobot = Robokind.connectRobot();
+            myGoalPositions = myRobot.getCurrentPositions();
+            mySpeaker = Robokind.connectSpeechService();
+            pw = new PrintWriter(new BufferedWriter(new FileWriter("C:\\Users\\zeno\\Documents\\NetBeansProjects\\store-positions.txt", true)));
+        } catch (Exception ex) {
+            Logger.getLogger(PDTest.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                br.close();
+            } catch (IOException ex) {
+                Logger.getLogger(PDTest.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 
     /*    @Override
@@ -51,10 +74,28 @@ public class PDTest extends MaxObject implements Executable {
         myRobot.move(myGoalPositions, 100);    
     }*/
     
+    private void writeGoalPositionsToFile() {
+        int count = 0;
+        for (Robot.JointId jid : myGoalPositions.keySet()) {
+            NormalizedDouble n = myGoalPositions.get(jid);
+            pw.println(jid + "\t" + n);
+            ++count;
+        }
+        post("writing to file "+count);
+      
+        pw.flush();
+    }
+    
     @Override
     protected void list(Atom content[]) {
         if (content[0].isString()) {
+            if (content[0].toString().equals("dump")) {
+                post("Dumped to file");
+                pw.println(content[1]);
+                writeGoalPositionsToFile();
+            }
             speak(content);
+            
         } else {
             float f = content[0].getFloat();
             String jointName = content[1].getString();
@@ -62,7 +103,7 @@ public class PDTest extends MaxObject implements Executable {
             post(f+"\t"+jointName+"\t"+jointId);
             Robot.JointId jid = new Robot.JointId(myRobot.getRobotId(), new Joint.Id(jointId)); 
             myGoalPositions.put(jid, new NormalizedDouble(f));
-            myRobot.move(myGoalPositions, 1);  
+            myRobot.move(myGoalPositions, 100);  
         }
     }
     
