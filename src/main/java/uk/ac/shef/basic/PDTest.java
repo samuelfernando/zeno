28b90e49d10a15ae1aa815ea79bbf2b28c3301f5
samf <samf@samf-PC>
2014-02-28 14:02:05 +0000

    added files + sound anim

--- src/main/java/uk/ac/shef/basic/PDTest.java
+++ src/main/java/uk/ac/shef/basic/PDTest.java
@@ -5,22 +5,33 @@ import com.cycling74.max.*;
 import static com.cycling74.max.MaxObject.post;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.jflux.api.core.Listener;
 import org.robokind.api.common.position.NormalizedDouble;
 import org.robokind.api.motion.Joint;
 import org.robokind.api.motion.Robot;
 import org.robokind.api.motion.messaging.RemoteRobot;
+import org.robokind.api.sensor.DeviceBoolEvent;
+import org.robokind.api.sensor.DeviceReadPeriodEvent;
+import org.robokind.api.sensor.FilteredVector3Event;
+import org.robokind.api.sensor.Vector3Event;
+import org.robokind.api.sensor.gpio.RemoteGpioServiceClient;
+import org.robokind.api.sensor.imu.RemoteAccelerometerServiceClient;
+import org.robokind.api.sensor.imu.RemoteCompassServiceClient;
+import org.robokind.api.sensor.imu.RemoteGyroscopeServiceClient;
 import org.robokind.api.speech.messaging.RemoteSpeechServiceClient;
 import org.robokind.client.basic.Robokind;
 import org.robokind.client.basic.UserSettings;
-import uk.ac.shef.settings.SetSettings;
+import org.robokind.impl.sensor.AccelerometerConfigRecord;
+import org.robokind.impl.sensor.CompassConfigRecord;
+import org.robokind.impl.sensor.DeviceReadPeriodRecord;
+import org.robokind.impl.sensor.GyroConfigRecord;
+import org.robokind.impl.sensor.HeaderRecord;
 
 public class PDTest extends MaxObject implements Executable {
     private static RemoteRobot myRobot;
@@ -39,8 +50,9 @@ public class PDTest extends MaxObject implements Executable {
             // SETTINGS - this is handled in SetSettings.java ////
             //////////////////////////////////////////////////////
             ///////////// use line 45 ////////////////////////////
-            SetSettings settings = new SetSettings(robotID, robotIP);
+            //SetSettings settings = new SetSettings(robotID, robotIP);
             //////////// instead of 47-55 ////////////////////////
+            
             UserSettings.setRobotId(robotID);
             UserSettings.setRobotAddress(robotIP);
             UserSettings.setAnimationAddress(robotIP);
@@ -55,6 +67,8 @@ public class PDTest extends MaxObject implements Executable {
             myGoalPositions = myRobot.getCurrentPositions();
             mySpeaker = Robokind.connectSpeechService();
             pw = new PrintWriter(new BufferedWriter(new FileWriter("C:\\Users\\samf\\Documents\\NetBeansProjects\\store-positions.txt", true)));
+           initSensors();  
+           declareIO(1, 18);
         } catch (Exception ex) {
             Logger.getLogger(PDTest.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
@@ -86,6 +100,132 @@ public class PDTest extends MaxObject implements Executable {
         pw.flush();
     }
     
+    
+    void initSensors() { 
+        RemoteGpioServiceClient sensors = Robokind.connectSensors();
+            DeviceReadPeriodEvent<HeaderRecord> readPeriod =
+                    new DeviceReadPeriodRecord();
+            HeaderRecord header = new HeaderRecord();
+            
+            System.out.println("Adding pin direction.");
+            
+            sensors.setPinDirection(2, true);
+            sensors.setPinDirection(4, true);
+            
+            System.out.println("Adding read period.");
+            
+            header.setFrameId(0);
+            header.setSequenceId(0);
+    //        header.setTimestamp(TimeUtils.now());
+            header.setTimestamp(0L);
+            readPeriod.setHeader(header);
+            readPeriod.setPeriod(100.0);
+            
+            sensors.setReadPeriod(readPeriod);
+            
+            System.out.println("Adding listener.");
+            
+            sensors.addListener(new TestGpioListener());
+            
+            System.out.println("Adding IMU.");
+            
+            RemoteAccelerometerServiceClient accel =
+                    Robokind.connectAccelerometer();
+            RemoteGyroscopeServiceClient gyro = Robokind.connectGyroscope();
+            RemoteCompassServiceClient compass = Robokind.connectCompass();
+            
+            readPeriod.setPeriod(1000.0);
+            
+            accel.setReadPeriod(readPeriod);
+            gyro.setReadPeriod(readPeriod);
+            compass.setReadPeriod(readPeriod);
+            
+            AccelerometerConfigRecord accelConfig = new AccelerometerConfigRecord();
+            GyroConfigRecord gyroConfig = new GyroConfigRecord();
+            CompassConfigRecord compassConfig = new CompassConfigRecord();
+            
+            accelConfig.setHeader(header);
+            gyroConfig.setHeader(header);
+            compassConfig.setHeader(header);
+            
+            accelConfig.setRegisterAddress(45);
+            accelConfig.setRegisterValue(8);
+            
+            gyroConfig.setCtl1(15);
+            gyroConfig.setCtl2(-1);
+            gyroConfig.setCtl3(-1);
+            gyroConfig.setCtl4(-1);
+            gyroConfig.setCtl5(-1);
+            
+            compassConfig.setAverage(3);
+            compassConfig.setBias(0);
+            compassConfig.setGain(7);
+            compassConfig.setRate(2);
+            
+            accel.sendConfig(accelConfig);
+            gyro.sendConfig(gyroConfig);
+            compass.sendConfig(compassConfig);
+            
+            accel.addListener(new TestAccelListener());
+            gyro.addListener(new TestGyroListener());
+            compass.addListener(new TestCompassListener());
+    }
+    
+        private static class TestGpioListener implements Listener<DeviceBoolEvent> {
+        public void handleEvent(DeviceBoolEvent t) {
+            //System.out.println(t.getChannelId() + ": " + (t.getBoolValue() ? "on" : "off"));
+        }
+    }
+    
+    private class TestAccelListener
+        implements Listener<FilteredVector3Event> {
+        public void handleEvent(FilteredVector3Event t) {
+            Vector3Event v = t.getFilteredVector();
+            Vector3Event r = t.getRawVector();
+           // System.out.println("Accelerometer (f): " + v.getX() + ", " + v.getY() + ", " + v.getZ());
+            //System.out.println("Accelerometer (r): " + r.getX() + ", " + r.getY() + ", " + r.getZ());
+            outlet(0, v.getX());
+            outlet(1, v.getY());
+            outlet(2, v.getZ());
+            outlet(3, r.getX());
+            outlet(4, r.getY());
+            outlet(5, r.getZ());
+            
+        }
+    }
+    
+    private class TestGyroListener
+        implements Listener<FilteredVector3Event> {
+        public void handleEvent(FilteredVector3Event t) {
+            Vector3Event v = t.getFilteredVector();
+            Vector3Event r = t.getRawVector();
+            //System.out.println("Gyroscope (f): " + v.getX() + ", " + v.getY() + ", " + v.getZ());
+            //System.out.println("Gyroscope (r): " + r.getX() + ", " + r.getY() + ", " + r.getZ());
+            outlet(6, v.getX());
+            outlet(7, v.getY());
+            outlet(8, v.getZ());
+            outlet(9, r.getX());
+            outlet(10, r.getY());
+            outlet(11, r.getZ());
+    
+        }
+    }
+    
+    private class TestCompassListener
+        implements Listener<FilteredVector3Event> {
+        public void handleEvent(FilteredVector3Event t) {
+            Vector3Event v = t.getFilteredVector();
+            Vector3Event r = t.getRawVector();
+            //System.out.println("Compass (f): " + v.getX() + ", " + v.getY() + ", " + v.getZ());
+            //System.out.println("Compass (r): " + r.getX() + ", " + r.getY() + ", " + r.getZ());
+            outlet(12, v.getX());
+            outlet(13, v.getY());
+            outlet(14, v.getZ());
+            outlet(15, r.getX());
+            outlet(16, r.getY());
+            outlet(17, r.getZ());
+        }
+    }
     @Override
     protected void list(Atom content[]) {
         if (content[0].isString()) {
